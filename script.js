744b9b6b737f98e835004d78787c6959575c6910
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2024-04-03 10:19:32 +0530

    allmost done

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-gsap.from("nav .logo", {
+var tl = gsap.timeline();
+
+tl.from("nav .logo", {
   y: -100,
   duration: 1,
   delay: 0.5,
   opacity: 0,
 });
-gsap.from("nav ul li:nth-child(1)", {
+tl.from("nav ul li", {
   y: 100,
   duration: 1,
-  delay: 0.4,
   opacity: 0,
   rotate: 180,
+  stagger: 0.4,
 });
-gsap.from("nav ul li:nth-child(2)", {
-  y: -100,
-  duration: 1,
-  delay: 0.4,
-  opacity: 0,
-  rotate: 180,
-});
-gsap.from("nav ul li:nth-child(3)", {
-  y: 100,
-  duration: 1,
-  delay: 0.4,
-  opacity: 0,
-  rotate: 180,
-});
-gsap.from("nav .buttons", {
+tl.from("nav .buttons", {
   x: 100,
   duration: 1,
-  delay: 1,
   opacity: 0,
+});
+
+tl.from(".main h1", {
+  y: 200,
+  duration: 1,
+  opacity: 0,
+  stagger: 0.5,
+});
+
+tl.from(".main>img", {
+  scale: 0,
+  opacity: 0,
+  stagger: 0.4,
+  duration: 1,
+});
+
+tl.from(".main>h5", {
+  scale: 0,
+  opacity: 0,
+  duration: 2,
+});
+
+tl.to("h5", {
+  y: 30,
+  repeat: -1,
+  duration: 0.5,
+  yoyo: true,
 });
